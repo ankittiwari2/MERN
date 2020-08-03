a525660963f7c4fe6e039240d30d793da723d97b
ankit tiwari <ankit.tiwari@mail.vinove.com>
2020-08-03 19:22:12 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
 var cors = require('cors')
 require("./config/db");
 
